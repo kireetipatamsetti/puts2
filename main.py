da9a40c676d704563b5a18be0d7360676f6324a7
kireetipatamsetti <paki17@student.bth.se>
2018-10-27 22:12:14 +0200

    operations for integers are made

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ def addition():
     result=value1+value2
     return '%d \n' % result
 
+@app.route('/sub')
+def substraction():
+    value1=request.args.get('A',default = 0, type = int)
+    value2=request.args.get('B',default = 0, type = int)
+    result=value1-value2
+    return '%d \n' % result
+
+@app.route('/mult')
+def multiplication():
+    value1=request.args.get('A',default = 0, type = int)
+    value2=request.args.get('B',default = 0, type = int)
+    result=value1*value2
+    return '%d \n' % result
+
+
 
 if __name__ == "__main__":
     app.run()
