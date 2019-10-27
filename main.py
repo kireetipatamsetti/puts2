e4dbcdbd3829c3c9a5e9f72dfe1829598b9d9bbc
kireetipatamsetti <43538568+kireetipatamsetti@users.noreply.github.com>
2019-10-27 18:23:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,94 @@
+  
 from flask import Flask, request
 from fractions import Fraction
 
 app = Flask(__name__)
 
+
+def take_inputs():
+    input1 = request.args.get('X', default=0, type=str)
+    try:
+        input1 = Fraction(input1)
+    except ZeroDivisionError:
+        return "X's denominator shouldn't be zero! \n"
+    except ValueError:
+        return "X's value should be a number (includes fraction, float, integer). \n"
+    input2 = request.args.get('Y', default=0, type=str)
+    try:
+        input2 = Fraction(input2)
+    except ZeroDivisionError:
+        return "Y's denominator shouldn't be zero! \n"
+    except ValueError:
+        return "Y's value should be a number (includes fraction, float, integer). \n"
+    return input1, input2
+
+
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
+    return 'Usage;\n<Operation>?X=<Input1>&Y=<Input2>\n'
 
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+    try:
+        input1, input2 = take_inputs()
+        result = input1 + input2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
+
 
 @app.route('/sub')
-def substraction():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1-value2
-    return '%d \n' % result
+def subtraction():
+    try:
+        input1, input2 = take_inputs()
+        result = input1 - input2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
 
-@app.route('/mult')
+
+@app.route('/mul')
 def multiplication():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1*value2
-    return '%d \n' % result
+    try:
+        input1, input2 = take_inputs()
+        result = input1 * input2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
+
 
 @app.route('/div')
 def division():
-    value1=request.args.get('A',default = 0, type = float)
-    value2=request.args.get('B',default = 0, type = float)
-    result=value1/value2
-    return '%.2f \n' % result 
-
-@app.route('/radd')
-def r_addition():
-    value1=request.args.get('A',default = 0, type = str)
-    num1,den1 = str(value1).split('/')
-    value2=request.args.get('B',default = 0, type = str)
-    num2,den2 = str(value2).split('/')
-    result=str(Fraction(int(num1),int(den1))+Fraction(int(num2),int(den2)))
-    return result
-
-@app.route('/rsub')
-def r_subtraction():
-    value1=request.args.get('A',default = 0, type = str)
-    num1,den1 = str(value1).split('/')
-    value2=request.args.get('B',default = 0, type = str)
-    num2,den2 = str(value2).split('/')
-    result=str(Fraction(int(num1),int(den1))-Fraction(int(num2),int(den2)))
-    return result
-
-@app.route('/rmult')
-def r_multiplication():
-    value1=request.args.get('A',default = 0, type = str)
-    num1,den1 = str(value1).split('/')
-    value2=request.args.get('B',default = 0, type = str)
-    num2,den2 = str(value2).split('/')
-    result=str(Fraction(int(num1),int(den1))*Fraction(int(num2),int(den2)))
-    return result 
-
-@app.route('/rdiv')
-def r_division():
-    value1=request.args.get('A',default = 0, type = str)
-    num1,den1 = str(value1).split('/')
-    value2=request.args.get('B',default = 0, type = str)
-    num2,den2 = str(value2).split('/')
-    result=str(Fraction(int(num1),int(den1))/Fraction(int(num2),int(den2)))
-    return result
+    try:
+        input1, input2 = take_inputs()
+        try:
+            result = ((input1)/(input2))
+        except ZeroDivisionError:
+            warning_msg = "Y's value shouldn't be zero! \n"
+            return warning_msg
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
 
 
 if __name__ == "__main__":
