aeac40e30d8e96a11e1dbd59cd97b1a4dcf9b95e
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 22:27:10 +0100

    Added testing for addition

---
+++ test.py
@@ -0,0 +1,16 @@
+import main
+import unittest
+
+class Tests(unittest.TestCase):
+	
+	def setUp(self):
+		main.app.testing = True
+		self.app = main.app.test_client()
+
+	def test_intadd(self):
+		result =  self.app.get('/add?A=2&B=5')
+            	self.assertEqual(b'7\n', result.data)
+            	self.assertNotEqual(b'6\n',result.data)
+
+if __name__ == '__main__':
+    unittest.main()
