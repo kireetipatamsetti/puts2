2b8209a7ee2b64a3592821824de0d3330174e3a3
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 22:16:12 +0100

    Subtraction module added

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
 	return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
-@app.route('/add')
-def addition():
+@app.route('/sub')
+def subtraction():
     try:
         a=request.args.get('A',default = 0, type = int)
     except ZeroDivisionError as e:
@@ -19,7 +19,7 @@ def addition():
     if a == 'None' or b == 'None' :
         return 'None'
     else:
-	result = a + b
+	result = a - b
         return str(result) + "\n"
 if __name__ == "__main__":
     app.run()
