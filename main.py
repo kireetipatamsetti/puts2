5470855f5df3420e932d434f52515a8e13911fad
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 22:21:46 +0100

    Division module added

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
 	return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
-@app.route('/mul')
-def multiplication():
+@app.route('/div')
+def division():
     try:
         a=request.args.get('A',default = 0, type = int)
     except ZeroDivisionError as e:
@@ -19,8 +19,11 @@ def multiplication():
     if a == 'None' or b == 'None' :
         return 'None'
     else:
-	result = a * b
-        return str(result) + "\n"
+	try:
+		result = a / b
+		return str(round(float(result),3)) + "\n"
+	except ZeroDivisionError as e:
+	        return "b shouldn't be zero!" + "\n"
 if __name__ == "__main__":
     app.run()
 
