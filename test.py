cb5248f50d3b5729939c13e1c09d09e7669fe617
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 23:00:13 +0100

    Tests for handling rationals added

--- test.py
+++ test.py
@@ -2,15 +2,27 @@ import main
 import unittest
 
 class Tests(unittest.TestCase):
-	
-	def setUp(self):
-		main.app.testing = True
-		self.app = main.app.test_client()
 
-	def test_intadd(self):
-		result =  self.app.get('/add?A=2&B=5')
-            	self.assertEqual(b'7\n', result.data)
-            	self.assertNotEqual(b'6\n',result.data)
+    def setUp(self):
+	main.app.testing = True
+	self.app = main.app.test_client()
+
+    def test_intadd(self):
+	result =  self.app.get('/add?A=2&B=5')
+        self.assertEqual(b'7.0\n', result.data)
+        self.assertNotEqual(b'6.0\n',result.data)
+
+    def test_floatadd(self):
+        result =  self.app.get('/add?A=2.3&B=3.3')
+        self.assertEqual(b'5.6\n', result.data)
+
+    def test_fracadd(self):
+        result =  self.app.get('/add?A=2/3&B=3/3')
+        self.assertEqual(b'1.667\n', result.data)
+
+    def test_negadd(self):
+        result =  self.app.get('/add?A=2.3&B=-3.3')
+        self.assertEqual(b'-1.0\n', result.data)
 
 if __name__ == '__main__':
     unittest.main()
