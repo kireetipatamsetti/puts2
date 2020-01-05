254f81bf5de40e1c9c66eb425620a1f5f850d47c
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 23:06:43 +0100

    Added tests

--- test.py
+++ test.py
@@ -2,15 +2,27 @@ import main
 import unittest
 
 class Tests(unittest.TestCase):
-	
-	def setUp(self):
-		main.app.testing = True
-		self.app = main.app.test_client()
 
-	def test_intsub(self):
-		result =  self.app.get('/sub?A=2&B=5')
-            	self.assertEqual(b'-3\n', result.data)
-            	self.assertNotEqual(b'6\n',result.data)
+    def setUp(self):
+	main.app.testing = True
+	self.app = main.app.test_client()
+
+    def test_intsub(self):
+	result =  self.app.get('/sub?A=3&B=5')
+        self.assertEqual(b'-2.0\n', result.data)
+        self.assertNotEqual(b'6.0\n',result.data)
+
+    def test_floatsub(self):
+        result =  self.app.get('/sub?A=3.3&B=3.3')
+        self.assertEqual(b'0.0\n', result.data)
+
+    def test_fracsub(self):
+        result =  self.app.get('/sub?A=247747/3&B=3/3')
+        self.assertEqual(b'82581.333\n', result.data)
+
+    def test_negsub(self):
+        result =  self.app.get('/sub?A=22929.29929&B=-3.3')
+        self.assertEqual(b'22932.599\n', result.data)
 
 if __name__ == '__main__':
     unittest.main()
