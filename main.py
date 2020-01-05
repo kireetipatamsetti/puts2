0c63cb7ba9ba7be308098d440b7868ca77f825d6
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 22:49:22 +0100

    Division module added

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ app = Flask(__name__)
 def index():
 	return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
-@app.route('/div')
-def division():
+@app.route('/sub')
+def multiplication():
     try:
-        a=request.args.get('A',default = 0, type = int)
+        a=request.args.get('A',default = 0, type = Fraction)
     except ZeroDivisionError as e:
         a='None'
     try:
-        b=request.args.get('B',default = 0, type = int)
+        b=request.args.get('B',default = 0, type = Fraction)
     except ZeroDivisionError as e:
         b='None'
     if a == 'None' or b == 'None' :
         return 'None'
     else:
 	try:
+		a = Fraction(a)
+		b = Fraction(b)
 		result = a / b
-		return str(round(float(result),3)) + "\n"
-	except ZeroDivisionError as e:
-	        return "b shouldn't be zero!" + "\n"
+        	return str(round(float(result),3)) + "\n"
+	except:
+		return "ERROR denominator part of A or B shouldnot be zero!\n"
 if __name__ == "__main__":
     app.run()
 
