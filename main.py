90b0769a1b1206cda6935c5a2321f12082ad3af5
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 22:07:15 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from fractions import Fraction
-
+from decimal import Decimal
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
-if __name__ == "__main __":
+	return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+if __name__ == "__main__":
     app.run()
+
+
+
