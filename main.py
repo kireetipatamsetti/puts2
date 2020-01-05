55d278dcf5ce01ee34ecd9e231fce78a4aa5b5e0
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 22:13:41 +0100

    Added integer manipulations

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+
+@app.route('/add')
+def addition():
+    try:
+        a=request.args.get('A',default = 0, type = int)
+    except ZeroDivisionError as e:
+        a='None'
+    try:
+        b=request.args.get('B',default = 0, type = int)
+    except ZeroDivisionError as e:
+        b='None'
+    if a == 'None' or b == 'None' :
+        return 'None'
+    else:
+	result = a + b
+        return str(result) + "\n"
 if __name__ == "__main__":
     app.run()
 
