24a32038c123a1edc261851c5cf82f105e892bca
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 23:25:26 +0100

    Tests added

--- test.py
+++ test.py
@@ -7,22 +7,26 @@ class Tests(unittest.TestCase):
 	main.app.testing = True
 	self.app = main.app.test_client()
 
-    def test_intmul(self):
-	result =  self.app.get('/mul?A=3&B=5')
-        self.assertEqual(b'15.0\n', result.data)
+    def test_intdiv(self):
+	result =  self.app.get('/div?A=3&B=5')
+        self.assertEqual(b'0.6\n', result.data)
         self.assertNotEqual(b'6.0\n',result.data)
 
-    def test_floatmul(self):
-        result =  self.app.get('/mul?A=3.3&B=3.3')
-        self.assertEqual(b'10.89\n', result.data)
+    def test_floatdiv(self):
+        result =  self.app.get('/div?A=3.3&B=3.3')
+        self.assertEqual(b'1.0\n', result.data)
 
-    def test_fracmul(self):
-        result =  self.app.get('/mul?A=247747/3&B=3/3')
+    def test_fracdiv(self):
+        result =  self.app.get('/div?A=247747/3&B=3/3')
         self.assertEqual(b'82582.333\n', result.data)
 
-    def test_negmul(self):
-        result =  self.app.get('/mul?A=22929.29929&B=-3.3')
-        self.assertEqual(b'-75666.688\n', result.data)
+    def test_negdiv(self):
+        result =  self.app.get('/div?A=22929.29929&B=-3.3')
+        self.assertEqual(b'-6948.273\n', result.data)
+
+    def test_zerodivisionerror(self):
+        result = self.app.get('/div?A=1/0&B=1')
+        self.assertEqual(b'None', result.data)
 
 if __name__ == '__main__':
     unittest.main()
