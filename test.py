49612d3e6c4c149d862ca3dea5d5b9e7694092a4
kireetipatamsetti <paki17@student.bth.se>
2020-01-05 23:09:29 +0100

    Added tests

--- test.py
+++ test.py
@@ -2,15 +2,27 @@ import main
 import unittest
 
 class Tests(unittest.TestCase):
-	
-	def setUp(self):
-		main.app.testing = True
-		self.app = main.app.test_client()
 
-	def test_intadd(self):
-		result =  self.app.get('/mul?A=2&B=5')
-            	self.assertEqual(b'10\n', result.data)
-            	self.assertNotEqual(b'6\n',result.data)
+    def setUp(self):
+	main.app.testing = True
+	self.app = main.app.test_client()
+
+    def test_intmul(self):
+	result =  self.app.get('/sub?A=3&B=5')
+        self.assertEqual(b'15.0\n', result.data)
+        self.assertNotEqual(b'6.0\n',result.data)
+
+    def test_floatmul(self):
+        result =  self.app.get('/sub?A=3.3&B=3.3')
+        self.assertEqual(b'10.89\n', result.data)
+
+    def test_fracmul(self):
+        result =  self.app.get('/sub?A=247747/3&B=3/3')
+        self.assertEqual(b'82582.333\n', result.data)
+
+    def test_negmul(self):
+        result =  self.app.get('/sub?A=22929.29929&B=-3.3')
+        self.assertEqual(b'-75666.688\n', result.data)
 
 if __name__ == '__main__':
     unittest.main()
