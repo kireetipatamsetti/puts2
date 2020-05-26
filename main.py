6ba523027d23faa52a03a3cb4a37f051846243b4
kireetipatamsetti <paki17@student.bth.se>
2020-05-26 13:18:49 +0200

    Rectify the error for inconsistent use of tabs and spaces in indentation

--- main.py
+++ main.py
@@ -1,88 +1,93 @@
 from flask import Flask, request
 from fractions import Fraction
 from decimal import Decimal
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-	return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+    return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+
 
 @app.route('/div')
 def division():
     try:
-        a=request.args.get('A',default = 0, type = Fraction)
+        a = request.args.get('A', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        a='None'
+        a = 'None'
     try:
-        b=request.args.get('B',default = 0, type = Fraction)
+        b = request.args.get('B', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        b='None'
-    if a == 'None' or b == 'None' :
+        b = 'None'
+    if a == 'None' or b == 'None':
         return 'None'
     else:
-	try:
+        try:
             a = Fraction(a)
             b = Fraction(b)
             result = a / b
-            return str(round(float(result),3)) + "\n"
-	except:
+            return str(round(float(result), 3)) + "\n"
+        except:
             return "None"
+
 
 @app.route('/add')
 def addition():
     try:
-        a=request.args.get('A',default = 0, type = Fraction)
+        a = request.args.get('A', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        a='None'
+        a = 'None'
     try:
-        b=request.args.get('B',default = 0, type = Fraction)
+        b = request.args.get('B', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        b='None'
-    if a == 'None' or b == 'None' :
+        b = 'None'
+    if a == 'None' or b == 'None':
         return 'None'
     else:
-	a = Fraction(a)
-	b = Fraction(b)
-	result = a + b
-        return str(round(float(result),3)) + "\n"
+        a = Fraction(a)
+        b = Fraction(b)
+        result = a + b
+        return str(round(float(result), 3)) + "\n"
+
 
 @app.route('/sub')
 def subtraction():
     try:
-        a=request.args.get('A',default = 0, type = Fraction)
+        a = request.args.get('A', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        a='None'
+        a = 'None'
     try:
-        b=request.args.get('B',default = 0, type = Fraction)
+        b = request.args.get('B', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        b='None'
-    if a == 'None' or b == 'None' :
+        b = 'None'
+    if a == 'None' or b == 'None':
         return 'None'
     else:
-	a = Fraction(a)
-	b = Fraction(b)
-	result = a - b
-        return str(round(float(result),3)) + "\n"
+        a = Fraction(a)
+        b = Fraction(b)
+        result = a - b
+        return str(round(float(result), 3)) + "\n"
+
 
 @app.route('/mul')
 def multiplication():
     try:
-        a=request.args.get('A',default = 0, type = Fraction)
+        a = request.args.get('A', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        a='None'
+        a = 'None'
     try:
-        b=request.args.get('B',default = 0, type = Fraction)
+        b = request.args.get('B', default=0, type=Fraction)
     except ZeroDivisionError as e:
-        b='None'
-    if a == 'None' or b == 'None' :
+        b = 'None'
+    if a == 'None' or b == 'None':
         return 'None'
     else:
-	a = Fraction(a)
-	b = Fraction(b)
-	result = a * b
-        return str(round(float(result),3)) + "\n"
+        a = Fraction(a)
+        b = Fraction(b)
+        result = a * b
+        return str(round(float(result), 3)) + "\n"
+
 
 if __name__ == "__main__":
     app.run()
-
-
-
--- test.py
+++ test.py
@@ -1,27 +1,28 @@
 import main
 import unittest
 
+
 class Tests(unittest.TestCase):
 
     def setUp(self):
-	main.app.testing = True
-	self.app = main.app.test_client()
+        main.app.testing = True
+        self.app = main.app.test_client()
 
     def test_intdiv(self):
-	result =  self.app.get('/div?A=3&B=5')
+        result = self.app.get('/div?A=3&B=5')
         self.assertEqual(b'0.6\n', result.data)
-        self.assertNotEqual(b'6.0\n',result.data)
+        self.assertNotEqual(b'6.0\n', result.data)
 
     def test_floatdiv(self):
-        result =  self.app.get('/div?A=3.3&B=3.3')
+        result = self.app.get('/div?A=3.3&B=3.3')
         self.assertEqual(b'1.0\n', result.data)
 
     def test_fracdiv(self):
-        result =  self.app.get('/div?A=247747/3&B=3/3')
+        result = self.app.get('/div?A=247747/3&B=3/3')
         self.assertEqual(b'82582.333\n', result.data)
 
     def test_negdiv(self):
-        result =  self.app.get('/div?A=22929.29929&B=-3.3')
+        result = self.app.get('/div?A=22929.29929&B=-3.3')
         self.assertEqual(b'-6948.273\n', result.data)
 
     def test_zerodivisionerror(self):
@@ -29,54 +30,54 @@ class Tests(unittest.TestCase):
         self.assertEqual(b'None', result.data)
 
     def test_intadd(self):
-	result =  self.app.get('/add?A=2&B=5')
+        result = self.app.get('/add?A=2&B=5')
         self.assertEqual(b'7.0\n', result.data)
-        self.assertNotEqual(b'6.0\n',result.data)
+        self.assertNotEqual(b'6.0\n', result.data)
 
     def test_floatadd(self):
-        result =  self.app.get('/add?A=2.3&B=3.3')
+        result = self.app.get('/add?A=2.3&B=3.3')
         self.assertEqual(b'5.6\n', result.data)
 
     def test_fracadd(self):
-        result =  self.app.get('/add?A=2/3&B=3/3')
+        result = self.app.get('/add?A=2/3&B=3/3')
         self.assertEqual(b'1.667\n', result.data)
 
     def test_negadd(self):
-        result =  self.app.get('/add?A=2.3&B=-3.3')
+        result = self.app.get('/add?A=2.3&B=-3.3')
         self.assertEqual(b'-1.0\n', result.data)
 
     def test_intsub(self):
-	result =  self.app.get('/sub?A=3&B=5')
+        result = self.app.get('/sub?A=3&B=5')
         self.assertEqual(b'-2.0\n', result.data)
-        self.assertNotEqual(b'6.0\n',result.data)
+        self.assertNotEqual(b'6.0\n', result.data)
 
     def test_floatsub(self):
-        result =  self.app.get('/sub?A=3.3&B=3.3')
+        result = self.app.get('/sub?A=3.3&B=3.3')
         self.assertEqual(b'0.0\n', result.data)
 
     def test_fracsub(self):
-        result =  self.app.get('/sub?A=247747/3&B=3/3')
+        result = self.app.get('/sub?A=247747/3&B=3/3')
         self.assertEqual(b'82581.333\n', result.data)
 
     def test_negsub(self):
-        result =  self.app.get('/sub?A=22929.29929&B=-3.3')
+        result = self.app.get('/sub?A=22929.29929&B=-3.3')
         self.assertEqual(b'22932.599\n', result.data)
 
     def test_intmul(self):
-	result =  self.app.get('/mul?A=3&B=5')
+        result = self.app.get('/mul?A=3&B=5')
         self.assertEqual(b'15.0\n', result.data)
-        self.assertNotEqual(b'6.0\n',result.data)
+        self.assertNotEqual(b'6.0\n', result.data)
 
     def test_floatmul(self):
-        result =  self.app.get('/mul?A=3.3&B=3.3')
+        result = self.app.get('/mul?A=3.3&B=3.3')
         self.assertEqual(b'10.89\n', result.data)
 
     def test_fracmul(self):
-        result =  self.app.get('/mul?A=247747/3&B=3/3')
+        result = self.app.get('/mul?A=247747/3&B=3/3')
         self.assertEqual(b'82582.333\n', result.data)
 
     def test_negmul(self):
-        result =  self.app.get('/mul?A=22929.29929&B=-3.3')
+        result = self.app.get('/mul?A=22929.29929&B=-3.3')
         self.assertEqual(b'-75666.688\n', result.data)
 
 
